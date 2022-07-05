d1a318fc614333044330446f7360be46158e54ba
wolfdog940 <oscarurrutia940@gmai..com>
2022-07-05 18:54:56 +0200

    ok

--- excusa.js
+++ excusa.js
@@ -11,12 +11,8 @@ return who [ramdom(4)] +" " +what [ramdom(4)]+" "+when [ramdom(5)];
 
 
 
-document.addEventListener("click", () => {
-//var texto=document.getElementByid("excuse")
-//texto.innerHTML=excusa();asi es lo mismo y no va
-    document.getElementById("excuse").innerHTML=excusa();
-  });
+window.onload =()=>{
+let texto=document.getElementById('excuse');
+texto.innerHTML=excusa()
 
-
-
-
+}
