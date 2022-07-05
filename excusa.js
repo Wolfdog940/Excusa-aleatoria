52515e7d437b64a44341a21d01f953947004b10e
wolfdog940 <oscarurrutia940@gmai..com>
2022-07-05 17:49:45 +0200

    ok

---
+++ excusa.js
@@ -0,0 +1,21 @@
+function ramdom(maxNumber) {
+return (Math.floor(Math.random() * maxNumber));
+}
+
+function excusa(){
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+return who [ramdom(4)] +" " +what [ramdom(4)]+" "+when [ramdom(5)];
+}
+
+
+
+document.addEventListener("click", () => {
+
+    document.getElementById("excuse").innerHTML=excusa();
+  });
+
+
+
+
