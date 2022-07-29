ee8ceca565600620cabd805c930884e7115d0531
wolfdog940 <oscarurrutia940@gmail.com>
2022-07-29 16:33:37 +0200

    otra excusa

--- excusa.js
+++ excusa.js
@@ -6,6 +6,7 @@ function excusa(){
 let who = ['the dog','my granma','his turtle','my bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+let hola=['before the class','right in time','when I finished','during my lunch','while I was praying'];
 return who [ramdom(4)] +" " +what [ramdom(4)]+" "+when [ramdom(5)];
 }
 
